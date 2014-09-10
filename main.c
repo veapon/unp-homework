5d90312b1eb3ce2bf473076f7fdf206e40808877
veapon <veapon88@gmail.com>
2014-09-10 18:55:19 +0800

    update

---
+++ main.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+#define FALSE 0
+#define TRUE 1
+
+int main(int argc, char **argv)
+{
+	int fd;
+	char *serv_ip = "127.0.0.1";
+	struct sockaddr_in serv;
+	memset(&serv, 0, sizeof(serv));
+	serv.sin_family = AF_INET;
+	serv.sin_port = htons(80);
+	if (1 != inet_pton(AF_INET, serv_ip, &serv.sin_addr)) {
+		printf("Unable to initialize socket\n");
+		return FALSE;
+	}
+
+	fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (fd == -1) {
+		printf("Unable to initialize socket\n");
+		return FALSE;
+	} 
+
+	if (connect(fd, (struct sockaddr *) &serv, sizeof(serv)) < 0) {
+		printf("Unable to connect server: %s", serv_ip);
+		return FALSE;
+	}
+
+
+	return TRUE;
+}
---
+++ net/socket.h
@@ -0,0 +1,5 @@
+#include <string.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h> 
+
